50d1d3778c5f5938ff4c207e3b871e7af028a61d
Rosamaria4512 <rmquilindo8@misena.edu.co>
2024-09-18 14:30:59 -0500

    reserva

---
+++ src/pages/CrearLugar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CrearLugar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/routes/AllRoutes.routes.jsx
+++ src/routes/AllRoutes.routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../pages/Login.jsx";
+import Reservas from "../pages/Reservas.jsx";
 
 
 const AllRoutes = () => {
@@ -8,6 +9,7 @@ const AllRoutes = () => {
         <Routes>
             <Route path="/" element={<Navigate to="/login"/>}/>
             <Route element={<Login/>} path="/login" />
+            <Route  path="/reservas" element={<Reservas/>}/>
         </Routes>
     );
 }
