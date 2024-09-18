8d1c83f8cbe31fc732c3ee3d6307987d7bb1937e
yesicazemanate <ymzemanate@soy.sena.edu.co>
2024-09-18 15:51:13 -0500

    formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <BrowserRouter>
       <AllRoutes/>
-
     </BrowserRouter>
   )
 }
--- src/components/FormReserva.jsx
+++ src/components/FormReserva.jsx
@@ -6,11 +6,31 @@ import {
    Typography,
    List, ListItem,
  } from "@material-tailwind/react";
+ import axios from 'axios'
 export const FormReserva = () => {
  const [fechas, setFechas]= useState()
- const [lugares ,setLugares]= useState()
+ const [lugares ,setLugares]= useState([])
+ const [lugar, setLugar]= useState()
+ const [fechaInicio, setFechaInicio]=useState()
+ const [fechaFinal, setFechaFinal] = useState()
+ const [idUsuario, setIdUsuario] = useState('66e9b0f7286af971576f3c06')
+console.log(lugar);
+ const handleSumit =async()=>{
+   const response= await axios.post(`${import.meta.env.VITE_BACKEND_URL}/reserva`,{
+      fechaInicio:fechaInicio,
+      fechaFin:fechaFinal,
+      lugar:lugar,
+      usuario:idUsuario
+   })
+   console.log(response);
+ }
  useEffect(()=>{
-const response = 
+   const lug=async()=>{
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/lugares`)
+      console.log(response.data.lugares);
+      setLugares(response.data.lugares)
+   }
+lug()
  },[])
  useEffect(()=>{
    const fechaHoy=()=>{
@@ -28,16 +48,19 @@ fechaHoy()
       <Typography variant="h4" color="blue-gray">
   Reserva lugares
       </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSumit}>
         <div className="mb-1 flex flex-col gap-6">
           <Typography variant="h6" color="blue-gray" className="-mb-3">
      Fecha de inicio
           </Typography>
           <Input
+          required
             size="lg"
             placeholder="name@mail.com"
             type='date'
             min={fechas}
+            value={fechaInicio}
+            onChange={(e)=>setFechaInicio(e.target.value)}
             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
@@ -47,27 +70,35 @@ fechaHoy()
      Fecha de fin
           </Typography>
           <Input
+             required
             size="lg"
             placeholder="name@mail.com"
-            type='date'
-            min={fechas}
+            type='datetime-local'
+            value={fechaFinal}
+            onChange={(e)=>setFechaFinal(e.target.value)}
             className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
+          
           <Typography variant="h6" color="blue-gray" className="-mb-3">
            Lugar
           </Typography>
-         <select name="" id="">
-        
-         <option value=""> F</option>
+          <select name=""  id=""value={lugar} onChange={(e)=>setLugar(e.target.value)}>
+            <option>selecciona el lugar</option>
+        {lugares&&lugares.map((lugar)=>(
+         <>
+       <option  value={lugar._id} >{lugar.nombre}</option>
+      </>
+
+))}
+</select>
  
-      </select>
           
         </div>
      
-        <Button className="mt-6" fullWidth>
+        <Button type='submit' className="mt-6" fullWidth>
         Guardar
         </Button>
        
