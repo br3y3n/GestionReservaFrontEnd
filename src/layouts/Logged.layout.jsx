741ed21194486463c60b3096dd678a006af84c4b
Juan Vidal <juanmvg2003@gmail.com>
2024-09-18 15:48:01 -0500

    logged layout

---
+++ src/layouts/Logged.layout.jsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom"
+
+import { AuthContext } from "../context/AuthContext.jsx"
+
+const LoggedLayout = ({ children }) => {
+
+    const menuItems = [
+        { title: "Reservas", path: "/reservas" },
+        { title: "Mis Reservas", path: "/mis-reservas" },
+        { title: "Lugares", path: "/crearlugar" },
+    ]
+
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
+    return (
+        <div className='w-screen h-screen bg-[#f2f2f2] flex'>
+        
+            <aside className='bg-dark w-[20vw] h-full p-5 text-white'>
+                <h6 className="text-center font-bold text-2xl mb-3">ReserYa</h6>
+                {
+                    user.tipoUsuario === "Administrador" ? (
+                        <ul>
+                            <li>
+                                <Link to="/reservas">
+                                    Reservas
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/crearlugar">
+                                    Crear Lugar
+                                </Link>
+                            </li>
+
+                        </ul>
+                    ) : (
+                        <ul>
+                            <li>
+                                <Link to="/reservas">
+                                    Reservas
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="mis-reservas">
+                                    Mis Reservas
+                                </Link>
+                            </li>
+                        </ul>
+                    )
+                }
+            </aside>
+            <main className='w-[70vw] p-5'>
+                { children }
+            </main>
+
+        </div>
+    );
+}
+
+export default LoggedLayout;
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import LoggedLayout from '../layouts/Logged.layout';
+
 const Test = () => {
     return (
-        <div>
-            asdasfds
-        </div>
+        <LoggedLayout>
+            <div>
+                asdasfds
+            </div>
+        </LoggedLayout>
     );
 }
 
