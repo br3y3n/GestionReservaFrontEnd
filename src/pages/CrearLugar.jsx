14b7dc2f64c3f31c79d14dcf5896a788630aab13
Rosamaria4512 <rmquilindo8@misena.edu.co>
2024-09-18 14:53:24 -0500

    registro lugar

--- src/pages/CrearLugar.jsx
+++ src/pages/CrearLugar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import {
+  Card,
+  Input,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+import axios from "axios";
 export default function CrearLugar() {
+  const [nombre, setNombre] = useState();
+  const [descripcion, setDescripcion] = useState();
+  const [direccion, setDireccion] = useState();
+  const registrarLugar = async () => {
+    const response = await axios.post("http://127.0.0.1:3031/lugares/", {
+      nombre,
+      descripcion,
+      direccion,
+    });
+    console.log(response.data);
+    alert(response.data.message);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="flex justify-center pt-10 bg-blue-gray-50 h-screen">
+      <Card className="px-5 py-8 h-max">
+        <Typography variant="h4" color="blue-gray">
+          Regiatra un lugar nuevo
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              nombre
+            </Typography>
+            <Input
+              onChange={(e) => setNombre(e.target.value)}
+              size="lg"
+              placeholder="ingresa un nombre"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              descripcion
+            </Typography>
+            <Input
+              onChange={(e) => setDescripcion(e.target.value)}
+              size="lg"
+              placeholder="ingresa una descripcion del lugar"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              direccion
+            </Typography>
+            <Input
+              onChange={(e) => setDireccion(e.target.value)}
+              size="lg"
+              placeholder="CALLE 0 # 00-00"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+          <Button className="mt-6" fullWidth onClick={() => registrarLugar()}>
+            Crear
+          </Button>
+        </form>
+      </Card>
+    </section>
+  );
 }
--- src/routes/AllRoutes.routes.jsx
+++ src/routes/AllRoutes.routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../pages/Login.jsx";
 import Reservas from "../pages/Reservas.jsx";
+import CrearLugar from "../pages/CrearLugar.jsx";
 
 
 const AllRoutes = () => {
@@ -10,6 +11,7 @@ const AllRoutes = () => {
             <Route path="/" element={<Navigate to="/login"/>}/>
             <Route element={<Login/>} path="/login" />
             <Route  path="/reservas" element={<Reservas/>}/>
+            <Route  path="/crearlugar" element={<CrearLugar/>}/>
         </Routes>
     );
 }
